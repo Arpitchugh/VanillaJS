1da016e68711879a03fb07afc8947262122b6c31
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-10-19 13:12:03 +0530

    use Memo hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import UseStateHook from './UseStateHooks';
+import UseEffectHooks from './UseEffectHooks';
+import UseMemoHooks from './UseMemoHooks';
 function App() {
 	return (
 		<div className='App'>
+			<p>------------------ useState ------------------</p>
 			<UseStateHook />
+			<p>------------------ useEffect ------------------</p>
+			<UseEffectHooks />
+			<p>------------------ useMemo ------------------</p>
+			<UseMemoHooks />
 		</div>
 	);
 }
---
+++ react-hooks/src/UseMemoHooks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UseMemoHooks = () => {
+  return (
+    <div>useMemoHooks</div>
+  )
+}
+
+export default UseMemoHooks
